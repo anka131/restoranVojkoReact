3c413cf8b6e289e92cd838625afd03909b20cace
anka131 <ankaknezevic99@gmail.com>
2024-08-06 11:47:51 +0200

    slike

--- src/pages/SingleBlog.js
+++ src/pages/SingleBlog.js
@@ -10,7 +10,7 @@ const SingleBlog = () => {
         <SingleBlogContent 
         subtitle="Fusce eget semper lacus. Sed ipsum purus, mollis non magna at, pellentesque pharetra augue."
         txt1="Fusce eget semper lacus. Sed ipsum purus, mollis non magna at, pellentesque pharetra augue. Quisque auctor urna elit, eu cursus massa ultricies vitae. Donec lorem diam, porttitor ac leo at, pretium faucibus metus"
-        img="img/steven-weeks-7uhSBPRtQGw-unsplash.jpg"
+        img="/img/steven-weeks-7uhSBPRtQGw-unsplash.jpg"
         txt2="Fusce eget semper lacus. Sed ipsum purus, mollis non magna at, pellentesque pharetra augue. Quisque auctor urna elit, eu cursus massa ultricies vitae. Donec lorem diam, porttitor ac leo at, pretium faucibus metus. Nunc sed metus nec quam tristique sodales vitae sit amet ante. Sed tempus sit amet urna sit amet tristique. Donec placerat nibh lacus, quis auctor libero mollis ut. Curabitur facilisis condimentum neque. Sed hendrerit ut justo sit amet commodo. Nunc eu enim nisi. Sed et bibendum leo, at accumsan risus."
         date="23.03.2024"
         txt3="Fusce eget semper lacus. Sed ipsum purus, mollis non magna at, pellentesque pharetra augue."
